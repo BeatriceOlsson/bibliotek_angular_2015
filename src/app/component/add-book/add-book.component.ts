9d64d6a9276f9d596bf40994ee2a8a2c8ab05122
Bea Olsson <beatrice-olsson@hotmail.com>
2025-07-03 19:01:56 +0200

    Created form component

--- src/app/component/add-book/add-book.component.ts
+++ src/app/component/add-book/add-book.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.css'
 })
 export class AddBookComponent {
-
+  newBookAdded = new FormControl('');
 }
