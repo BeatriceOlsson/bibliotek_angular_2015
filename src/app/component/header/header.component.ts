80ead69eec474d126ee9ef82804030a1a2c69c05
BeatriceLydh <beatrice-olsson@hotmil.com>
2025-11-16 18:55:59 +0100

    created a latest book funtion on home page for latest book

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Book, BooksService } from '../../service/books.service';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,30 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-menyOpen = false;
+  menyOpen = false;
+  books = signal<Book[]>([]);
+  router = inject(Router);
+  booksService = inject(BooksService);
+
+  constructor() {
+    this.booksService.getBooks().subscribe(data => {
+      this.books.set(data);
+    });
+  }
 
   toggleMenu() {
     this.menyOpen = !this.menyOpen;
   }
   closeMenu() {
     this.menyOpen = false;
+  }
+
+  latestBook() {
+    const latest = [...this.books()].sort((a, b) =>
+      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())[0];
+    console.log(latest);
+    if (latest) {
+      this.router.navigate(['app-book-ditail', latest.id]);
+    };
   }
 }
